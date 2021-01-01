209923deed356684f7c3cf3c7fd0ba63128c6255
luke robertson <robertson.stuff@gmail.com>
2021-01-01 21:35:28 +1100

    full update

--- PropertyCalc.py
+++ PropertyCalc.py
@@ -2,6 +2,8 @@
 price = int(input("Give me the price of this place! "))
 rent = int(input("and what's the rent per week? "))
 bodyC = int(input("and the body corporate per quarter? "))
+RatesLand = int(input("What do the land rates look like per quarter? "))
+RatesWater = int(input("Finally, I'll need the water rates per quarter? "))
 
 
 def conv(x):
@@ -10,12 +12,18 @@ def rconv(x):
     return(rent * 52 / price * 100)
 def pconv(y):
     return(((rent * 52) - (bodyC * 4)) / price * 100)
+def lconv(z):
+    return(((rent * 52) - (bodyC * 4) - (RatesLand * 4)) / price * 100)
+def wconv(y):
+    return(((rent * 52) - (bodyC * 4) - (RatesLand * 4) - (RatesWater * 4)) / price * 100)
 
-
+Yearly_Water = wconv(RatesWater)
+Yearly_Land = lconv(RatesLand)
 yearly_yield_BC = pconv(bodyC)
 yearly_rent = rconv(rent)
 yearly_yield = conv(price)
  
 print(f"\nThis is your rental yield excluding Body Corporate {yearly_rent}% ")
 print(f"This is your rental yield including Body Corporate {yearly_yield_BC}% ")
-print(f"The rent needs to be ${yearly_yield} per week to be at 7%.\n")
+print(f"Body Corporate and land rates is {Yearly_Land}% yield. ")
+print(f"\nSo you're final yield will be at {Yearly_Water}% compared to {yearly_rent}% ")
